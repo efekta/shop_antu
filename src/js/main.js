cf062eabdd53ef27e236b22ab67c0d84c36340ad
Ptashkina Liliya <efekta9@gmail.com>
2019-06-06 21:33:28 +0300

    no message

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+import './vendor';
+/* -------------------------------- 
+
+Main Components Menu Sidebar
+
+-------------------------------- */
+jQuery(document).ready(function(){
+	var accordionsMenu = $('.cd-accordion-menu');
+
+	if( accordionsMenu.length > 0 ) {
+		
+		accordionsMenu.each(function(){
+			var accordion = $(this);
+			//detect change in the input[type="checkbox"] value
+			accordion.on('change', 'input[type="checkbox"]', function(){
+				var checkbox = $(this);
+				console.log(checkbox.prop('checked'));
+				( checkbox.prop('checked') ) ? checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300) : checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
+			});
+		});
+	}
+	$('.cd-accordion-menu a').on('click', function(event) {
+		event.preventDefault();
+		$(this).find('.swithes-icon-svg svg').toggleClass('switches-icon-svg_down');
+	});
+});
---
+++ src/js/vendor.js
@@ -0,0 +1,12 @@
+import '@babel/polyfill';
+import svg4everybody from 'svg4everybody';
+import $ from 'jquery';
+import 'bootstrap';
+import './vendor/fontawesome'
+svg4everybody();
+
+window.$ = $;
+window.jQuery = $;
+
+require('ninelines-ua-parser');
+

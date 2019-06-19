af1a63625d8400476c1501915408518876b8c877
Ptashkina Liliya <efekta9@gmail.com>
2019-06-19 17:19:51 +0300

    no message

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -198,19 +198,28 @@ $(document).ready(function () {
 
 	  responsive: [
 	    {
+	      breakpoint: 992,
+	      settings: {
+	        arrows: false,
+	        // centerMode: true,
+	        // centerPadding: '40px',
+	        slidesToShow: 2
+	      }
+	    },
+	    {
 	      breakpoint: 768,
 	      settings: {
 	        arrows: false,
-	        centerMode: true,
+	        // centerMode: true,
 	        // centerPadding: '40px',
-	        slidesToShow: 3
+	        slidesToShow: 2
 	      }
 	    },
 	    {
 	      breakpoint: 480,
 	      settings: {
 	        arrows: false,
-	        centerMode: true,
+	        // centerMode: true,
 	        // centerPadding: '40px',
 	        slidesToShow: 1
 	      }
